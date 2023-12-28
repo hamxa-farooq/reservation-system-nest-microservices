5f9bae6cb2989d9974adcec9f56a002e13b52ede
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-12-28 09:25:46 +0500

    Add pino-logger and payload validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
