7fba1a001b3bb3b86e5c4b4110a058cae9ec45a2
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-12-30 01:46:11 +0500

    Refactor config module logic so each microservice has its own set of envs

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,9 +3,23 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from './user/user.module';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [UserModule, LoggerModule],
+  imports: [
+    UserModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        MONGODB_URI: joi.string().required(),
+        JWT_SECRET: joi.string().required(),
+        JWT_EXPIRY: joi.string().required(),
+        PORT: joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
