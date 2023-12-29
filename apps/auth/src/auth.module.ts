f2863f28adbddeaeab98a267609f4dbdd8d42382
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-12-29 23:20:31 +0500

    Add validation and pino logger in users microservice

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from './user/user.module';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, LoggerModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
