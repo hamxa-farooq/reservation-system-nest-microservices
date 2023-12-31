d20c98cb3ceea959571265ce6b9ef0e2fca5cff4
Hamza Farooq <hamxa.farooq.47@gmail.com>
2023-12-31 07:17:12 +0500

    Add config module

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        PORT: joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, ConfigService],
 })
 export class PaymentsModule {}
