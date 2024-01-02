aabfbc0ae28ac5b76808f99991ffb846ee756af0
Hamza Farooq <hamxa.farooq.47@gmail.com>
2024-01-02 20:40:05 +0500

    Add pino logger

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
 import { ConfigService } from '@nestjs/config';
 import { Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
     },
   });
 
+  app.useLogger(app.get(Logger));
   await app.startAllMicroservices();
 }
 bootstrap();
--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -3,9 +3,11 @@ import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as joi from 'joi';
+import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
+    LoggerModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: joi.object({
