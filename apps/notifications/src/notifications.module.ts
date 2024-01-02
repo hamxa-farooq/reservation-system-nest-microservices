bb674056e99c6a07370c5a03c8434707660cca9a
Hamza Farooq <hamxa.farooq.47@gmail.com>
2024-01-02 20:09:31 +0500

    Add config module

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        PORT: joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, ConfigService],
 })
 export class NotificationsModule {}
