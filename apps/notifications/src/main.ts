0598f25b27a5e4a4fdeac86c5b1ebb32ae64e0ac
Hamza Farooq <hamxa.farooq.47@gmail.com>
2024-01-02 20:38:00 +0500

    Make notifications app a standalone microservice

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
+
+  await app.startAllMicroservices();
 }
 bootstrap();
